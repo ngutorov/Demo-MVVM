13ecf55c6e67862fa42ce7697b7006ab05c71e7e
Nikolay Gutorov <ngutorov@gmail.com>
2020-03-23 05:55:42 -0700

    Initial Commit

---
+++ Demo-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo-MVVM
+//
+//  Created by Nikolay Gutorov on 3/23/20.
+//  Copyright © 2020 Nikolay Gutorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
